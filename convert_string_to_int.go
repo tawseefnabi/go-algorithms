428b0b25221b49418e439b43fe43fc9c56490226
tawseefnabi9 <tawseefnabi9@gmail.com>
2023-01-02 09:44:48 +0530

    adding parseInt func

--- convert_string_to_int.go
+++ convert_string_to_int.go
@@ -15,4 +15,15 @@ func main() {
 	strVar := "100"
 	intVar, err := strconv.Atoi(strVar)
 	fmt.Println(intVar, err, reflect.TypeOf(intVar))
+
+	// ParseInt() Function
+	// ParseInt interprets a string s in the given base (0, 2 to 36) and bit size (0 to 64) and
+	// returns the corresponding value i. This function accepts a string parameter, convert it into a
+	//  corresponding int type based on a base parameter. By default, it returns Int64 value.
+
+	intVar8, err := strconv.ParseInt(strVar, 0, 8)
+	fmt.Println(intVar8, err, reflect.TypeOf(intVar))
+	intVar16, err := strconv.ParseInt(strVar, 0, 16)
+	fmt.Println(intVar16, err, reflect.TypeOf(intVar))
+
 }
