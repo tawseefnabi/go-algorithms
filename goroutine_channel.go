c61597d790035dec250883bb7ae3dc3421537e00
tawseefnabi9 <tawseefnabi9@gmail.com>
2023-01-04 23:51:35 +0530

    adding goroutines with channel

---
+++ goroutine_channel.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"sync"
+)
+
+// WaitGroup is used to wait for the program to finish goroutines.
+var wg sync.WaitGroup
+
+func responseSize(url string, nums chan int) {
+	// Schedule the call to WaitGroup's Done to tell goroutine is completed.
+	defer wg.Done()
+
+	response, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("len", len(body))
+	// Send value to the unbuffered channel
+	nums <- len(body)
+}
+
+func main() {
+	nums := make(chan int) // Declare a unbuffered channel
+	wg.Add(1)
+	go responseSize("https://www.golangprograms.com", nums)
+	fmt.Println(<-nums) // Read the value from unbuffered channel
+	wg.Wait()
+	close(nums) // Closes the channel
+}
