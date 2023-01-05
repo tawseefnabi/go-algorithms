bd5c5212c1346a24625032eb17590b9e32715975
tawseefnabi9 <tawseefnabi9@gmail.com>
2023-01-06 00:06:27 +0530

    file reading functions

--- create_directry.go
+++ create_directry.go
@@ -7,12 +7,10 @@ import (
 
 func main() {
 	_, err := os.Stat("test")
-
 	if os.IsNotExist(err) {
-		errDir := os.MkdirAll("test", 0755)
+		errDir := os.Mkdir("test", 0755)
 		if errDir != nil {
-			log.Fatal(err)
+			log.Fatal(errDir)
 		}
-
 	}
 }
--- log.go
+++ log.go
@@ -20,8 +20,8 @@ func main() {
 	// Println writes to the standard logger
 	log.Println("main started")
 
-	// Fatalln is Println() followed by a call to os.Exit(1)
 	log.Fatalln("fatal message")
+	// Fatalln is Println() followed by a call to os.Exit(1)
 
 	// Panicln is Println() followed by a call to panic()
 	log.Panicln("panic message")
