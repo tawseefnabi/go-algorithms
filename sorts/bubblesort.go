711790751c1a48d6fceb8cfe9471e03235ab8f3c
tawseefnabi9 <tawseefnabi9@gmail.com>
2023-01-19 10:53:05 +0530

    bubble sort in go

---
+++ sorts/bubblesort.go
@@ -0,0 +1,13 @@
+package sorts
+
+// Bubble is a simple generic definition of Bubble sort algorithm.
+func BubbleSort(array []int) []int {
+	for i := 0; i < len(array)-1; i++ {
+		for j := 0; j < len(array)-i-1; j++ {
+			if array[j] > array[j+1] {
+				array[j], array[j+1] = array[j+1], array[j]
+			}
+		}
+	}
+	return array
+}
---
+++ sorts/sorts_test.go
@@ -0,0 +1,12 @@
+package sorts
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_BubbleSort(t *testing.T) {
+	array := []int{43, 11, 18, 8, 3, 17}
+	bubbleSortArray := []int{3, 8, 11, 17, 18, 43}
+	assert.Equal(t, BubbleSort(array), bubbleSortArray)
+}
