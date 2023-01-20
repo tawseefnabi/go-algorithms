f2bef778863f0a47b9495e40c25a8ae3594413fa
tawseefnabi9 <tawseefnabi9@gmail.com>
2023-01-20 13:09:12 +0530

    adding set menthods like getItems, delete

--- datastrucutures/sets/set/set_test.go
+++ datastrucutures/sets/set/set_test.go
@@ -13,3 +13,14 @@ func Test_New(t *testing.T) {
 	set.Add(4)
 	assert.Equal(t, set.Len(), 4)
 }
+func Test_Add(t *testing.T) {
+	set := New(1, 2, 3)
+	set.Add(5)
+	assert.Equal(t, set.Len(), 4)
+}
+
+func Test_Delete(t *testing.T) {
+	set := New(1, 2, 3)
+	set.Delete(2)
+	assert.Equal(t, set.Len(), 2)
+}
