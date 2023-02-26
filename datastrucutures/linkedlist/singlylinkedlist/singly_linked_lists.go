ff51d63a5d5d7c48e9c478a4a7f056cad91758dc
tawseefnabi9 <tawseefnabi9@gmail.com>
2023-02-26 09:40:50 +0530

    linked list

--- datastrucutures/linkedlist/singlylinkedlist/singly_linked_lists.go
+++ datastrucutures/linkedlist/singlylinkedlist/singly_linked_lists.go
@@ -2,6 +2,7 @@ package singlylinkedlist
 
 import (
 	"errors"
+	"fmt"
 )
 
 var (
@@ -27,7 +28,12 @@ type SLLNode[T any] struct {
 type SinglyLinkedList[T any] struct {
 	count int
 	head  *SLLNode[T]
-	last  *SLLNode[T]
+	tail  *SLLNode[T]
+}
+
+// IsEmpty checks if the list is empty
+func (s *SinglyLinkedList[T]) isEmpty() bool {
+	return s.Size() == 0
 }
 
 // Size returns size of the list
@@ -37,18 +43,63 @@ func (s *SinglyLinkedList[T]) Size() int {
 
 // GetFirstValue returns first value in the list
 func (s *SinglyLinkedList[T]) GetFirstValue() (res T, err error) {
-
+	if s.isEmpty() {
+		return res, errEmptyList
+	}
 	return s.head.Value, nil
 }
 
 // Add add to the list
 func (s *SinglyLinkedList[T]) Add(values ...T) {
 	for _, value := range values {
+		fmt.Println("==", value, s.Size())
 		s.insertAt(s.Size(), value)
 	}
 }
 
 // InsertAt insert value at specific index in the list
 func (s *SinglyLinkedList[T]) insertAt(index int, value T) error {
+	fmt.Println("insertAt", index, s, s.count)
+	if index < 0 || index > s.count {
+		fmt.Println("errors")
+		return errIndexOutOfBounds
+	}
+	element := &SLLNode[T]{Value: value}
+	fmt.Println("ele", element, s)
+	if s.isEmpty() {
+		fmt.Println("index is empty")
+		s.head = element
+		s.tail = element
+		s.count++
+		element = nil
+		return nil
+	}
+	if index == 0 {
+		fmt.Println("index is 0")
+		element.Next = s.head
+		s.head = element
+		s.count++
+		element = nil
+		return nil
+	}
+	if index == s.count {
+		fmt.Println("index is s.count")
+		s.tail.Next = element
+		s.tail = element
+		s.count++
+		element = nil
+		return nil
+	}
+	current, err := s.getNode(index - 1)
+	fmt.Println("current", current, err)
+
 	return nil
+}
+
+func (s *SinglyLinkedList[T]) getNode(index int) (*SLLNode[T], error) {
+	if index < 0 || index >= s.count {
+		return nil, errIndexOutOfBounds
+	}
+	fmt.Println("cureent", s)
+	return nil, nil
 }
--- datastrucutures/linkedlist/singlylinkedlist/singly_linked_lists_test.go
+++ datastrucutures/linkedlist/singlylinkedlist/singly_linked_lists_test.go
@@ -1,14 +1,26 @@
 package singlylinkedlist
 
 import (
-	"fmt"
-	"reflect"
-	"testing"
-
+	// "fmt"
+	// "reflect"
 	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 func TestSinglyLinked_GetFirstValue(t *testing.T) {
 	list := New[string]()
-	fmt.Println("++++", list)
+	_, err := list.GetFirstValue()
+	assert.NotNil(t, err)
+	list.Add("abc")
+	value, err := list.GetFirstValue()
+	assert.Nil(t, err)
+	assert.Equal(t, value, "abc")
+	list.Add("def")
+	value, err = list.GetFirstValue()
+	assert.Nil(t, err)
+	assert.Equal(t, value, "abc")
+	list.insertAt(0, "xyz")
+	value, err = list.GetFirstValue()
+	assert.Nil(t, err)
+	assert.Equal(t, value, "xyz")
 }
